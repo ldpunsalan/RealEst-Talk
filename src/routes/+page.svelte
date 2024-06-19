ac2d37726baef55419753c57e0df1b2fb36fee49
ldpunsalan <ldpunsalan@up.edu.ph>
2024-06-19 15:03:15 +0800

    feat: update website design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,109 +1,121 @@
 <script>
-  import Counter from "./Counter.svelte";
   import Card from "./Card.svelte";
-  import welcome from "$lib/images/svelte-welcome.webp";
-  import welcome_fallback from "$lib/images/svelte-welcome.png";
 </script>
 
 <svelte:head>
   <title>Home</title>
-  <meta name="description" content="Svelte demo app" />
+  <meta name="description" content="Real(i)ty Check" />
 </svelte:head>
 
-<section class="hero-container">
-  <div class="flex xs:flex-col xs:items-center md:flex-row md:space-x-10">
-    <div class="flex flex-col">
-      <h1
-        style="font-size: 5rem;"
-        class="text-left leading-none font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#cba6f7] to-[#89b4fa]"
+<main>
+  <section class="hero-container px-20">
+    <Card>
+      <div class>
+        <div class="flex flex-col">
+          <h1
+            style="font-size: 5rem;"
+            class="text-center leading-none font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#cba6f7] to-[#89b4fa] px-4"
+          >
+            Real<span class="text-[#cdd6f4]">(i)</span>ty Check
+          </h1>
+          <br />
+          <p class="text-center">
+            An analysis of real estate prices across different cities in the
+            Philippines <br /> by
+            <span
+              class="bg-clip-text text-transparent bg-gradient-to-r from-[#cba6f7] to-[#89b4fa]"
+              >RealEst Talk</span
+            >
+          </p>
+        </div>
+      </div></Card
+    >
+  </section>
+  <section id="overview" class="px-20">
+    <div class="flex flex-row items-center space-x-4">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="var(--color-theme-1)"
+        class="size-12"
       >
-        Real<span class="text-white">(i)</span>ty Check
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+        />
+      </svg>
+      <h1
+        class="text-left bg-clip-text bg-clip-text text-transparent bg-gradient-to-r from-[#cba6f7] to-[#89b4fa]"
+      >
+        Overview
       </h1>
-      <p>
-        An analysis of real estate prices across different cities in the
-        Philippines
-      </p>
-      <p>by RealEst Talk</p>
     </div>
-
-    <Card id="Background">
-      <h1
-        class="text-left font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#89b4fa] to-[#cba6f7]"
-      >
-        Background
-      </h1>
-      <p class="text-left">
-        The Philippine real estate market is influenced by various factors,
-        including economic growth, infrastructure development, and population
-        density. There is a pressing issue of housing affordability,
-        particularly in urban areas, where real estate prices continue to rise,
-        and making homeownership increasingly inaccessible for many segments of
-        the population. This has led to housing insecurity, forced evictions,
-        informal settlements, and social exclusion, exacerbating inequalities
-        and hindering sustainable urban development.
-      </p>
-    </Card>
-  </div>
-</section>
-<br /><br />
-<section>
-  <h1
-    class="font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#89b4fa] to-[#cba6f7]"
-  >
-    Overview
-  </h1>
-  <div class="flex flex-row space-x-4">
-    <Card>
-      <h2>Objective</h2>
-      <p>
-        The aim is to assess the affordability of housing, state of urban
-        planning and economic health of the different regions in the country.
-      </p>
-    </Card>
-    <Card>
-      <h2>Problem</h2>
-      <p>
-        Affordable housing is increasingly becoming a problem amidst the rising
-        prices of real estate in across the country.
-      </p>
-    </Card>
-    <Card>
-      <h2>Solution</h2>
-      <p>
-        Pinpoint areas where housing is becoming inaccessible and expensive
-        through a statistical comparative analysis of real estate prices and the
-        price-to-floor area ratio among property types across different
-        Philippine cities.
-      </p>
-    </Card>
-  </div>
-</section>
-<br /><br />
-<section>
-  <h1
-    class="font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#89b4fa] to-[#cba6f7]"
-  >
-    Hypotheses
-  </h1>
-  <div class="flex flex-row space-x-4">
-    <Card>
-      <h2>Null</h2>
-      <p>
-        There is no significant difference between prices of similar types of
-        real estate in highly urbanized cities compared to non-highly urbanized
-        areas.
-      </p>
-    </Card>
-    <Card>
-      <h2>Alternative</h2>
-      <p>
-        Real estate in more highly urbanized cities of the Philippines are more
-        expensive than their counterparts in non-highly urbanized areas of the
-        country.
-      </p>
-    </Card>
-  </div>
-</section>
+    <p class="text-center">
+      The Philippine real estate market is influenced by various factors,
+      including economic growth, infrastructure development, and population
+      density. There is a pressing issue of housing affordability, particularly
+      in urban areas, where real estate prices continue to rise, and making
+      homeownership increasingly inaccessible for many segments of the
+      population. This has led to housing insecurity, forced evictions, informal
+      settlements, and social exclusion, exacerbating inequalities and hindering
+      sustainable urban development.
+    </p>
+    <div class="flex flex-row space-x-4">
+      <Card>
+        <h2>Objective</h2>
+        <p>
+          The aim is to assess the affordability of housing, state of urban
+          planning and economic health of the different regions in the country.
+        </p>
+      </Card>
+      <Card>
+        <h2>Problem</h2>
+        <p>
+          Affordable housing is increasingly becoming a problem amidst the
+          rising prices of real estate in across the country.
+        </p>
+      </Card>
+      <Card>
+        <h2>Solution</h2>
+        <p>
+          Pinpoint areas where housing is becoming inaccessible and expensive
+          through a statistical comparative analysis of real estate prices and
+          the price-to-floor area ratio among property types across different
+          Philippine cities.
+        </p>
+      </Card>
+    </div>
+  </section>
+  <br /><br />
+  <section id="overview" class="px-20">
+    <h1
+      class="font-bold bg-clip-text text-transparent bg-gradient-to-r from-[#89b4fa] to-[#cba6f7]"
+    >
+      Hypotheses
+    </h1>
+    <div class="flex flex-row space-x-4">
+      <Card>
+        <h2>Null</h2>
+        <p>
+          There is no significant difference between prices of similar types of
+          real estate in highly urbanized cities compared to non-highly
+          urbanized areas.
+        </p>
+      </Card>
+      <Card>
+        <h2>Alternative</h2>
+        <p>
+          Real estate in more highly urbanized cities of the Philippines are
+          more expensive than their counterparts in non-highly urbanized areas
+          of the country.
+        </p>
+      </Card>
+    </div>
+  </section>
+</main>
 
 <style>
   section {
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -5,77 +5,134 @@
 </script>
 
 <header>
-  <div class="corner">
-    <img src={logo} alt="Logo" />
-  </div>
+  <div class="flex fixed top-4 px-4 w-screen w-auto z-50">
+    <nav
+      class="flex flex-col rounded-full bg-[#1e1e2e99] border-[1px] border-[#89b4fa55] p-1 py-3"
+    >
+      <div>
+        <a
+          aria-current={$page.url.pathname === "/" ? "page" : undefined}
+          href="#overview"
+          class="flex justify-center items-center text-center flex-col"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+          </svg>
 
-  <nav class="fixed z-20 w-full">
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-    </svg>
-    <ul>
-      <li aria-current={$page.url.pathname === "/" ? "page" : undefined}>
-        <a href="/">Top</a>
-      </li>
-      <li aria-current={$page.url.pathname === "#top" ? "page" : undefined}>
-        <a href="#top">Background</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">Data</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">Methods</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">Results</a>
-      </li>
-      <li aria-current={$page.url.pathname === "/about" ? "page" : undefined}>
-        <a href="/about">Team</a>
-      </li>
-    </ul>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-    </svg>
-  </nav>
+          <div>Overview</div></a
+        >
+      </div>
+      <div>
+        <a
+          aria-current={$page.url.pathname === "/" ? "page" : undefined}
+          href="#data"
+          class="flex justify-center items-center text-center flex-col"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z"
+            />
+          </svg>
 
-  <div class="corner">
-    <a href="https://github.com/ldpunsalan/RealEst-Talk">
-      <img src={github} alt="GitHub" />
-    </a>
+          <div>Data</div></a
+        >
+      </div>
+      <div>
+        <a
+          aria-current={$page.url.pathname === "/" ? "page" : undefined}
+          href="#methods"
+          class="flex justify-center items-center text-center flex-col"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M6 13.5V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m12-3V3.75m0 9.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 3.75V16.5m-6-9V3.75m0 3.75a1.5 1.5 0 0 1 0 3m0-3a1.5 1.5 0 0 0 0 3m0 9.75V10.5"
+            />
+          </svg>
+
+          <div>Methods</div></a
+        >
+      </div>
+      <div>
+        <a
+          aria-current={$page.url.pathname === "/" ? "page" : undefined}
+          href="#results"
+          class="flex justify-center items-center text-center flex-col"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5M9 11.25v1.5M12 9v3.75m3-6v6"
+            />
+          </svg>
+
+          <div>Results</div></a
+        >
+      </div>
+      <div>
+        <a
+          aria-current={$page.url.pathname === "/" ? "page" : undefined}
+          href="#team"
+          class="flex justify-center items-center text-center flex-col"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-6"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z"
+            />
+          </svg>
+          <div>Team</div></a
+        >
+      </div>
+    </nav>
   </div>
 </header>
 
 <style>
-  header {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .corner {
-    width: 3em;
-    height: 3em;
-  }
-
-  .corner a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
-
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
-  }
-
-  nav {
-    display: flex;
-    justify-content: center;
-    --background: #11111b;
-  }
-
   svg {
     width: 2em;
     height: 3em;
@@ -98,38 +155,23 @@
     background: var(--background);
     background-size: contain;
   }
-
-  li {
-    position: relative;
-    height: 100%;
-  }
-
-  li[aria-current="page"]::before {
-    --size: 6px;
-    content: "";
-    width: 0;
-    height: 0;
-    position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--color-theme-1);
-  }
-
   nav a {
     display: flex;
     height: 100%;
     align-items: center;
-    padding: 0 0.5rem;
+    padding: 0.5rem 0.5rem;
     color: var(--color-text);
-    font-size: 0.8rem;
-    text-transform: uppercase;
+    font-size: 0.7rem;
     letter-spacing: 0.1em;
     text-decoration: none;
     transition: color 0.2s linear;
   }
 
   a:hover {
+    color: var(--color-theme-1);
+  }
+
+  svg:hover {
     color: var(--color-theme-1);
   }
 </style>
